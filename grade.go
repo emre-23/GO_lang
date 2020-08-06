d7532792cee209d1632e4387f0aea7fa20f36412
YunusEMRE <53182424+emre-23@users.noreply.github.com>
2020-08-06 13:26:19 +0300

    Update grade.go

--- grade.go
+++ grade.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("Bişeyler yaz")
 	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(input)
 }
