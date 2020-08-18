888a6f84d7ecf09511a70c16e5b52a0b0249be29
YunusEMRE <53182424+emre-23@users.noreply.github.com>
2020-08-18 15:20:04 +0300

    Create pointers1.go

---
+++ pointers1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*var myInt int
+	var myIntPointer *int
+	myIntPointer = &myInt
+	fmt.Println(myIntPointer)
+	var myFloat float64
+	fmt.Println(reflect.TypeOf(&myFloat)) */
+	myInt := 4
+	fmt.Println(myInt)
+	myIntPointer := &myInt
+	*myIntPointer = 8
+	fmt.Println(myIntPointer)  // write the pointer itself
+	fmt.Println(*myIntPointer) // write the value at pointer
+	fmt.Println(myInt)         //Updated th pointer value
+
+	myFloat := 8.7
+	myFloatPointer := &myFloat
+	fmt.Println(myFloatPointer)  // write the pointer itself
+	fmt.Println(*myFloatPointer) // write the value at pointer
+
+}
