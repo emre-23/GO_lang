96d503e2f6693763c928bdbdd1afe9bae85fcf5a
YunusEMRE <53182424+emre-23@users.noreply.github.com>
2020-11-24 14:41:12 +0300

    Delete ldap_sync.sh

--- ldap_sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-oc login -u sdxmwadm -p Xr8Ey5mm
-export KUBECONFIG=/root/openshift/installation/acptst/auth/kubeconfig
-oc adm groups sync --whitelist=/root/openshift/confs/ldap/group_sync_whitelist.txt  --sync-config=/root/openshift/confs/ldap/ldap_group_sync.yaml --confirm
